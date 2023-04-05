e8c2daa95a8e8d858262f4ba79e2b93f3447d684
Aleksey-Dr <aprel181818@gmail.com>
2023-04-05 10:42:09 +0300

    Added "Fetch countries"
    1. Project is restructured.
    1) Added new html files:
    - fetch-users.html;
    - fetch-countries.html.
    2) Added new js files:
    - fetch-users.js;
    - fetch-countries.js;
    - fetchCountries.js.
    3) Script from java-script.js moved to fetch-users.js.
    2. Added "Fetch countries".
    1) Added a task "Fetch countries" to fetch-countries.js and fetchCountries.js.
    2) Module with function for fetch attributes of countries in fetchCountries.js.
    3. Added anybody styles.

---
+++ js/fetch-countries.js
@@ -0,0 +1,90 @@
+// Import fetchCountries
+import { fetchCountries } from './fetchCountries.js';
+// console.log(fetchCountries);
+
+// Find elements
+// Create object with elements
+const refs = {
+    inputCountry: document.querySelector('#search-box'),
+    countryInfo: document.querySelector('.country-info'),
+    countryList: document.querySelector('.country-list'),
+};
+// console.log(refs.inputCountry);
+// console.log(refs.countryInfo);
+// console.log(refs.countryList);
+
+// Added CONST for delay
+const DEBOUNCE_DELAY = 300;
+
+// Added debounce function
+const debounce = (func, waitTime) => {
+    let timeOut;
+    return () => {
+        clearTimeout(timeOut);
+        timeOut = setTimeout(func, waitTime);
+    };
+};
+
+// Added listener for input
+refs.inputCountry.addEventListener('input',
+    debounce( 
+        onSearch, DEBOUNCE_DELAY)
+);
+
+function onSearch(evt) {
+    // stop reboot page
+    // evt.preventDefault();
+    const countryName = refs.inputCountry.value.trim();
+    console.log(countryName);
+    // Cleare markup when input clean
+    if (!countryName) {
+        refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+        return;
+    }
+    // Show message when error
+    if (countryName != '') {
+        fetchCountries(countryName)
+            .then(renderCountry)
+            .catch(error => {
+                refs.countryList.innerHTML = '';
+                refs.countryInfo.innerHTML = '';
+                console.log("Oops, there is no country with that name", error)
+            });
+    }
+}
+
+// Added function for rendering markup when found country/countries
+function renderCountry(countries) {
+    // console.log(countries);
+    // switzerland
+    // When found 1 a country
+    if (countries.length === 1) {
+        refs.countryList.innerHTML = '';
+        const markup = countries.map((country) => {
+            return `<img src="${country.flags.svg}" alt="${country.official}" width='80'>
+        <h2>${country.name.official}</h2>
+        <p><b>Capital</b>: ${country.capital[0]}</p>
+        <p><b>Population</b>: ${country.population}</p>
+        <p><b>Languages</b>: ${Object.values(country.languages)}</p>`;
+        })
+            .join("");
+        refs.countryInfo.innerHTML = markup;
+    // When found from 2 to 10 countries
+    } else if (countries.length > 1 && countries.length < 11) {
+        refs.countryInfo.innerHTML = '';
+        const markup = countries.map((country) => {
+            return `<li class="item-card">
+            <img src="${country.flags.svg}" alt="${country.flags.alt}" width="30" class="img-item">
+            ${country.name.official}
+            </li>`;
+        })
+            .join("");
+        refs.countryList.innerHTML = markup;
+    // When found more 10 countries
+    } else if (countries.length > 10) {
+        refs.countryList.innerHTML = '';
+        // Message when found too many countries. List cleaned
+        return console.log("Too many matches found. Please enter a more specific name.");
+    }
+}
